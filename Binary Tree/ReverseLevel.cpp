82e7d4b0ebb651272be3a21589d30051e5e836bd
Rishubh Kumar <82128737+Rishubh03@users.noreply.github.com>
2021-09-24 17:04:29 +0530

    Create ReverseLevel.cpp

---
+++ Binary Tree/ReverseLevel.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node *left, *right;
+    Node(int item){
+        this->data = item;
+        left=right=NULL;
+    }
+};
+
+vector<int> levelorder(Node *node){
+    vector<int> ans;
+    if(!node) return ans;
+    queue<Node*>q;
+    
+    q.push(node);
+    while(!q.empty()){
+        Node *temp = q.front();
+        ans.push_back(temp->data);
+       
+        if(temp->right) q.push(temp->right);
+        if(temp->left) q.push(temp->left);
+        q.pop();
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
+}
+
+int main()
+{
+    Node *node = new Node(10);
+    node->left = new Node(20);
+    node->right = new Node(30);
+    node->left->left = new Node(40);
+    node->left->right = new Node(50);
+    node->right->left = new Node(60);
+    node->right->right = new Node(40);
+    
+    vector<int> arr = levelorder(node);
+    
+    for(int i =0; i<=sizeof(arr)/sizeof(arr[0]);i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
