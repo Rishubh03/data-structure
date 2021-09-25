019d84f0bc1b6e1d122d213d002b9e10d2349e8f
Rishubh Kumar <82128737+Rishubh03@users.noreply.github.com>
2021-09-25 17:30:06 +0530

    Create RightRotate.cpp

---
+++ Array/RightRotate.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+
+void rotate(int n, int a[],int d)
+{
+  for(int i=0; i<d; i++)
+  {
+    int temp;
+    for(int j=0; j<n-1; j++)
+    {
+      temp =a[j];
+      a[j]=a[n-1];
+      a[n-1]=temp;
+    }
+  }
+
+  for(int i=0; i<n; i++)
+  {
+    cout<<a[i]<<" ";
+  }
+}
+
+int main() {
+  int n,d;
+  cin>>n;
+  int a[n];
+  for(int i=0; i<n ;i++)
+  {
+    cin>>a[i];
+  }
+  cin>>d;
+  
+  rotate(n,a,d);
+
+  return 0;
+}
